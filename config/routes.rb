ae44b4d9ad6edc052d50b3e65c2ebd5ad9f5d98e
dbzguy56 <dbzguy56@gmail.com>
2018-05-02 13:50:19 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
-  
+
   get '/mastermind', to: 'mastermind#new'
   get '/mini_reddit', to: 'mini_reddit#index'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 end
